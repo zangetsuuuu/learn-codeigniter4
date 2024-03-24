d2dff4574e0b83baf3591ed75983cd5d0228a54c
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-24 23:20:32 +0700

    feat: add view for pages

---
+++ app/Views/pages/contact.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @var CodeIgniter\View\View $this
+*/
+?>
+
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <h1>Contact</h1>
+</div>
+
+<?= $this->endSection(); ?>
