047cd8ed8ad0f0e0a299ee306b5a542a2d57f653
Rafif Athallah <rafifathallah99@gmail.com>
2024-03-24 23:17:16 +0700

    feat: add new routes for page and comics

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,12 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Page COntroller Route
+$routes->get('/home', 'Page::home');
+$routes->get('/about', 'Page::about');
+$routes->get('/contact', 'Page::contact');
+
+// Comics Controller Route
+$routes->get('/comics', 'Comics::index');
+$routes->get('/comics/(:segment)', 'Comics::detail/$1');
