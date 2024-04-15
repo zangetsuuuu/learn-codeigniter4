38224a0e77dcd7cfa94eab50188c1aad5139eb2f
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-15 15:51:09 +0700

    Add: Page for add comic

---
+++ app/Views/comics/create.php
@@ -0,0 +1,49 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1 class="mb-4">Tambah Komik</h1>
+            <form action="/comics/save" method="post">
+                <?= csrf_field(); ?>
+                <div class="form-group row mb-4">
+                    <label for="judul" class="col-sm-2 col-form-label">Judul</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control <?= ($validation) ? 'is-invalid' : '' ?>" id="judul" name="judul" value="<?= old('judul'); ?>" autofocus>
+                        <div class="invalid-feedback">
+                            <?= ($validation) ? $validation->getError('judul') : '' ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label for="penulis" class="col-sm-2 col-form-label">Penulis</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="penulis" name="penulis" value="<?= old('penulis'); ?>">
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label for="penerbit" class="col-sm-2 col-form-label">Penerbit</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="penerbit" name="penerbit" value="<?= old('penerbit'); ?>">
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <label for="sampul" class="col-sm-2 col-form-label">Sampul</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="sampul" name="sampul">
+                    </div>
+                </div>
+                <div class="form-group row mb-4">
+                    <div class="col-sm-10 offset-sm-2">
+                        <button type="submit" class="btn btn-primary">Tambah</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</div>
+
+<?= $this->endSection(); ?>
