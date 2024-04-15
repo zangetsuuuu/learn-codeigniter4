257816243eae4b9af27f483a19b30a5281da1d67
Rafif Athallah <rafifathallah99@gmail.com>
2024-04-15 15:48:05 +0700

    Feat: Add create, save, edit, update, delete

--- app/Controllers/Comics.php
+++ app/Controllers/Comics.php
@@ -17,7 +17,7 @@ class Comics extends BaseController
     {
         $data = [
             'title' => 'Comics',
-            'comic' => $this->comicsModel->getComics()
+            'comics' => $this->comicsModel->getComics()
         ];
 
         return view('comics/index', $data);
@@ -27,9 +27,116 @@ class Comics extends BaseController
     {
         $data = [
             'title' => 'Detail Komik',
-            'comic' => $this->comicsModel->getComics($slug)
+            'comics' => $this->comicsModel->getComics($slug)
         ];
 
+        if (empty($data['comics'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Judul komik ' . $slug . ' tidak ditemukan');
+        }
+
         return view('comics/detail', $data);
+    }
+
+    public function create()
+    {
+        $data = [
+            'title' => 'Form Tambah Komik',
+            'validation' => session()->getFlashdata('validation')
+        ];
+
+        return view('comics/create', $data);
+    }
+
+
+    public function save()
+    {
+        $config = [
+            'judul' => [
+                'rules' => 'required|is_unique[comics.judul]',
+                'errors' => [
+                    'required' => 'Judul komik tidak boleh kosong!',
+                    'is_unique' => 'Judul komik sudah tersedia!'
+                ]
+            ],
+        ];
+
+        if (!$this->validate($config)) {
+            return redirect()->to('/comics/create')->withInput()->with('validation', $this->validator);
+        }
+
+        $data = [
+            'judul' => $this->request->getVar('judul'),
+            'slug' => url_title($this->request->getVar('judul'), '-', true),
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ];
+
+        $this->comicsModel->save($data);
+
+        session()->setFlashdata('Pesan', 'Data berhasil ditambahkan!');
+
+        return redirect()->to('comics');
+    }
+
+    public function edit($slug)
+    {
+        $data = [
+            'title' => 'Form Tambah Komik',
+            'validation' => session()->getFlashdata('validation'),
+            'comics' => $this->comicsModel->getComics($slug)
+        ];
+
+        return view('comics/edit', $data);
+    }
+
+    public function update($id)
+    {
+        $oldComic = $this->comicsModel->getComics($this->request->getVar('slug'));
+
+        if ($oldComic['judul'] == $this->request->getVar('judul')) {
+            $title_rule = 'required';
+        } else {
+            $title_rule = 'required|is_unique[comics.judul]';
+        }
+
+
+        $config = [
+            'judul' => [
+                'rules' => $title_rule,
+                'errors' => [
+                    'required' => 'Judul komik tidak boleh kosong!',
+                    'is_unique' => 'Judul komik sudah tersedia!'
+                ]
+            ],
+        ];
+
+        if (!$this->validate($config)) {
+            return redirect()->to('/comics/edit/' . $this->request->getVar('slug'))->withInput()->with('validation', $this->validator);
+        }
+
+        $data = [
+            'id' => $id,
+            'judul' => $this->request->getVar('judul'),
+            'slug' => url_title($this->request->getVar('judul'), '-', true),
+            'penulis' => $this->request->getVar('penulis'),
+            'penerbit' => $this->request->getVar('penerbit'),
+            'sampul' => $this->request->getVar('sampul')
+        ];
+
+        $this->comicsModel->save($data);
+
+        session()->setFlashdata('Pesan', 'Data berhasil diubah!');
+
+        return redirect()->to('/comics');
+    }
+
+    public function delete($id)
+    {
+        $this->comicsModel->delete($id);
+
+        session()->setFlashdata('Pesan', 'Data berhasil dihapus!');
+
+        return redirect()->to('/comics');
     }
 }
